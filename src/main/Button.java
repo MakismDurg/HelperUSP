06f6c1b19eb3677cf72b16b92543eb842a9933eb
MakismDurg <maksim_pavlov_1993@inbox.ru>
2023-09-29 09:18:37 +0300

    Вторая редакция. Выделил кнопки в отдельный класс. Стоило ли оно того? Как знать.

--- src/main/Button.java
+++ src/main/Button.java
@@ -8,26 +8,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Button extends JButton{
-    private static TextArea a;
-    private static TextArea b;
+    private static TextArea windowRequestBeforeCleaning;
+    private static TextArea windowRequestAfterCleaning;
 
-    public Button(TextArea a, TextArea b) {
-        this.a = a;
-        this.b = b;
+    public Button(TextArea windowRequestBeforeCleaning, TextArea windowRequestAfterCleaning) {
+        Button.windowRequestBeforeCleaning = windowRequestBeforeCleaning;
+        Button.windowRequestAfterCleaning = windowRequestAfterCleaning;
     }
+
     static ActionListener insertRequest = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            a.setText(null);
-            a.paste();
-            b.setText(null);
+            windowRequestBeforeCleaning.setText(null);
+            windowRequestBeforeCleaning.paste();
+            windowRequestAfterCleaning.setText(null);
         }
     };
     static ActionListener cleanRequest = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String request = Launcher.stringClean(a.getText());
-            b.setText(request);
+            String request = Launcher.stringClean(windowRequestBeforeCleaning.getText());
+            windowRequestAfterCleaning.setText(request);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             StringSelection stringSelection = new StringSelection(request);
             clipboard.setContents(stringSelection, null);
